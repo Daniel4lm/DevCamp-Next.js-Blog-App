de3895196ee9f0612733198435df3e8eacba2445
Daniel Molnar <daniel4molnar@gmail.com>
2023-08-24 17:29:17 +0200

    Add new icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -525,14 +525,14 @@ const CloseIcon = () => {
             <path
                 d="M18 6L6 18"
                 stroke="currentColor"
-                strokeWidth="2"
+                strokeWidth="1.6"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
             <path
                 d="M6 6L18 18"
                 stroke="currentColor"
-                strokeWidth="2"
+                strokeWidth="1.6"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
@@ -1071,6 +1071,41 @@ const SmallUserIcon = () => {
     )
 }
 
+const MoonIcon = () => {
+    return (
+        <svg
+            id="theme-toggle-dark-icon"
+            width="24"
+            height="24"
+            className="md:w-7 md:h-7 hidden dark:block"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+        </svg>
+    )
+}
+
+const SunIcon = () => {
+    return (
+        <svg
+            id="theme-toggle-light-icon"
+            className="w-6 h-6 md:w-7 md:h-7 dark:hidden"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                fillRule="evenodd"
+                clipRule="evenodd"
+            >
+            </path>
+        </svg>
+    )
+}
+
 const CopyButton = () => {
     return (
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -1097,4 +1132,54 @@ const CopyButton = () => {
     )
 }
 
-export { AppIcon, AvatarIcon, ChatIcon, CloseUploadIcon, CloseIcon, CloseSideMenuIcon, CommentIcon, ConfirmIcon, CopyButton, CopyLinkIcon, DeleteIcon, DownloadIcon, EditIcon, EmptyHeartIcon, GlobeIcon, HambButton, HeartIcon, HomeIcon, HomeIconFill, LikeIcon, LinkIcon, LoaderIcon, LogoutIcon, NewIcon, NewIconFill, NewPostIcon, HidePassIcon, OptsIcon, PinIcon, PostIcon, PostTagIcon, ReadTimeIcon, ReplyIcon, SavedListIcon, ScrollToTopIcon, SettingsIcon, SettingsIcon2, ShowPassIcon, SmallListIcon, SmallUserIcon, TagIcon, TagListIcon, ThemeIcon, UnsupportedIcon, UploadImage, UserIcon, WorkMenuIcon }
+export {
+    AppIcon,
+    AvatarIcon,
+    ChatIcon,
+    CloseUploadIcon,
+    CloseIcon,
+    CloseSideMenuIcon,
+    CommentIcon,
+    ConfirmIcon,
+    CopyButton,
+    CopyLinkIcon,
+    DeleteIcon,
+    DownloadIcon,
+    EditIcon,
+    EmptyHeartIcon,
+    GlobeIcon,
+    HambButton,
+    HeartIcon,
+    HomeIcon,
+    HomeIconFill,
+    LikeIcon,
+    LinkIcon,
+    LoaderIcon,
+    LogoutIcon,
+    MoonIcon,
+    NewIcon,
+    NewIconFill,
+    NewPostIcon,
+    HidePassIcon,
+    OptsIcon,
+    PinIcon,
+    PostIcon,
+    PostTagIcon,
+    ReadTimeIcon,
+    ReplyIcon,
+    SavedListIcon,
+    ScrollToTopIcon,
+    SettingsIcon,
+    SettingsIcon2,
+    ShowPassIcon,
+    SmallListIcon,
+    SmallUserIcon,
+    SunIcon,
+    TagIcon,
+    TagListIcon,
+    ThemeIcon,
+    UnsupportedIcon,
+    UploadImage,
+    UserIcon,
+    WorkMenuIcon
+}
