348605084e41ce2cc9e87e8871fce84ba2bfc93c
Daniel Molnar <daniel4molnar@gmail.com>
2023-08-24 10:41:50 +0200

    Add password reset tokens, change user role

---
+++ prisma/migrations/20230824084032_create_reset_password_tokens_change_users_role/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - The `role` column on the `users` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "passwordUpdatedAt" TIMESTAMP(3),
+DROP COLUMN "role",
+ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'USER';
+
+-- CreateTable
+CREATE TABLE "ResetPasswordToken" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "resetedAt" TIMESTAMP(3),
+
+    CONSTRAINT "ResetPasswordToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ResetPasswordToken_token_key" ON "ResetPasswordToken"("token");
+
+-- AddForeignKey
+ALTER TABLE "ResetPasswordToken" ADD CONSTRAINT "ResetPasswordToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
