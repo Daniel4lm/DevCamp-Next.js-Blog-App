67d72f86224887f99c61ee4b0f53b1b65fb15f69
Daniel Molnar <daniel4molnar@gmail.com>
2023-08-24 17:26:47 +0200

    Fix post tags page

--- src/app/(main)/posts/tags/[tag]/page.tsx
+++ src/app/(main)/posts/tags/[tag]/page.tsx
@@ -2,9 +2,9 @@
 
 import { useState } from "react"
 import { useInfiniteQuery } from "@tanstack/react-query"
-import { SmallPostCard } from "@/components/CardsComponent"
+import { SmallPostCard } from "@/components/posts-comments/CardsComponent"
 import Skeleton from "@/components/skeletons/Skeleton"
-import { Post, Tag, User } from "@prisma/client"
+import { UserPost } from "@/models/Post"
 
 type PageProps = {
     params: {
@@ -13,11 +13,7 @@ type PageProps = {
 }
 
 interface PostResultsProps {
-    posts: (Post & {
-        comments: Comment[]
-        author: User
-        tags: Tag[]
-    })[]
+    posts: UserPost[]
     totalPages: number
     totalCount: number
 }
@@ -31,12 +27,12 @@ async function getPosts(page: number, tag: string) {
 export default function PostsListByTag({ params }: PageProps) {
 
     const { tag } = params
-    const [page, setPage] = useState(0)
     const [totalCount, setTotalCount] = useState(0)
     const [totalPages, setTotalPages] = useState(0)
 
     const {
         fetchNextPage,
+        isFetching,
         hasNextPage,
         data: userPosts,
         isLoading,
@@ -44,13 +40,13 @@ export default function PostsListByTag({ params }: PageProps) {
         queryKey: [`posts-tag-${tag}`],
         queryFn: async ({ pageParam = 0 }) => {
             const results = await getPosts(pageParam, tag)
-            setPage(pageParam + 1)
             setTotalPages(results.totalPages)
             setTotalCount(results.totalCount)
             return results.posts
         },
         getNextPageParam: (lastPage, allPages) => {
-            return page < totalPages ? page : undefined;
+            const nextPage = allPages.length
+            return nextPage < totalPages ? nextPage : undefined
         },
     })
 
@@ -58,13 +54,24 @@ export default function PostsListByTag({ params }: PageProps) {
         <>
             <div id="top" className="px-2 my-4 flex flex-col items-center p-4 text-slate-600 dark:text-white shadow-lg dark:shadow-none">
                 <h2 className="mx-auto text-xl sm:text-3xl text-center font-semibold uppercase pt-4">#{tag}</h2>
-                <h3 className="mx-auto text-base sm:text-lg text-center font-light my-2">
-                    {totalCount > 0 ? `Results of ${totalCount} ${totalCount === 1 ? "post" : "posts"}` : `There is no results for ${tag}`}
-                </h3>
+                {isLoading || isFetching ? (
+                    <>
+                        <Skeleton classes='text width-50 my-2' />
+                    </>
+                ) : (
+                    <h3 className="mx-auto text-base sm:text-lg text-center font-light my-2">
+                        {totalCount > 0 ? `Results of ${totalCount} ${totalCount === 1 ? "post" : "posts"}` : `There is no results for ${tag}`}
+                    </h3>
+                )}
             </div>
             <section className="w-full sm:w-10/12 md:w-2/3 xl:w-3/6 min-h-[45vh] dark:text-slate-100 mx-auto px-2 xs:px-4 md:px-0 pb-8 mb-1 mt-6 sm:mt-0">
                 <div id="posts-list-slug" className="flex flex-col gap-y-4 mt-4">
-                    {isLoading ? (
+                    {
+                        userPosts?.pages.map((page) => {
+                            return page.map((post) => (<SmallPostCard key={post.id} postData={post as UserPost} />))
+                        })
+                    }
+                    {isLoading || isFetching ? (
                         <>
                             {[...Array(4).keys()].map(i => {
                                 return (
@@ -78,11 +85,6 @@ export default function PostsListByTag({ params }: PageProps) {
                             })}
                         </>
                     ) : null}
-                    {
-                        userPosts?.pages.map((page) => {
-                            return page.map((post) => (<SmallPostCard key={post.id} postData={post} />))
-                        })
-                    }
                 </div>
                 <div className="mt-4 p-4">
                     <button
