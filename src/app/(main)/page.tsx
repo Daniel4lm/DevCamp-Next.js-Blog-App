bfbe597ef5cf0d3ad3a0ada6bac1599103ad1375
Daniel Molnar <daniel4molnar@gmail.com>
2023-08-30 09:19:39 +0200

    Redesign root page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-between px-8 lg:px-24 py-10 lg:py-20 mt-14">
-      <div className="z-10 flex w-full max-w-5xl items-center justify-center bg-gray-100 dark:bg-slate-600 rounded-xl my-6">
+    <section className="flex min-h-screen flex-col items-center justify-around px-8 lg:px-24 py-10 lg:py-20 mt-14">
+      <div className="z-10 flex w-full max-w-5xl items-center justify-center bg-gray-100 dark:bg-slate-600/40 rounded-xl my-6">
         <p className="w-full text-lg text-center lg:text-2xl font-semibold dark:from-inherit lg:w-auto lg:rounded-xl p-4">
           Your powerful rich Instacamp Blog app.
         </p>
@@ -13,7 +13,7 @@ export default function Home() {
 
       <div className="relative flex place-items-center">
         <Image
-          className="relative dark:invert"
+          className="relative "
           src="/landing.svg"
           alt="Landing Page Logo"
           width={120}
@@ -22,16 +22,44 @@ export default function Home() {
         />
       </div>
 
-      <div className='flex flex-col items-center text-center gap-2 my-2'>
-        <Link href="/posts?dialog=y" className="text-2xl underline">Go to Products with Modal</Link>
+      <div className="my-4 grid text-center lg:mb-0 md:grid-cols-2 lg:text-left gap-2">
+        <Link
+          href="/feed"
+          className="group rounded-xl border border-transparent px-5 py-4 transition-colors hover:shadow-lg hover:border-slate-200 hover:bg-slate-50 hover:dark:border-slate-700 hover:dark:bg-slate-600/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Blog feed page{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore other articles you mayfind usefull.
+          </p>
+        </Link>
 
-        <Link href="/posts" className="text-2xl underline">Go to Products without Modal</Link>
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-xl border border-transparent px-5 py-4 transition-colors hover:shadow-lg hover:border-slate-200 hover:bg-slate-50 hover:dark:border-slate-700 hover:dark:bg-slate-600/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 md:grid-cols-2 lg:text-left gap-2">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="group rounded-xl border border-transparent px-5 py-4 transition-colors hover:shadow-lg hover:border-slate-200 hover:bg-slate-50 hover:dark:border-slate-700 hover:dark:bg-slate-600/30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -48,7 +76,7 @@ export default function Home() {
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          className="group rounded-xl border border-transparent px-5 py-4 transition-colors hover:shadow-lg hover:border-slate-200 hover:bg-slate-50 hover:dark:border-slate-700 hover:dark:bg-slate-600/30 hover:dark:bg-opacity-30"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -60,40 +88,6 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
       </div>
