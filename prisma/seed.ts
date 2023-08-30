83ccf1ca5956452c2e61a300e10e9906ba859547
Daniel Molnar <daniel4molnar@gmail.com>
2023-08-30 09:08:25 +0200

    Add more posts to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,12 @@
-import { encryptPassword } from '@/lib/helperFunctions'
 import { PrismaClient } from '@prisma/client'
+import { genSaltSync, hashSync } from 'bcryptjs'
 const prisma = new PrismaClient()
+
+export function encryptPassword(receivedPassword: string) {
+    let saltRounds = 10
+    let salt = genSaltSync(saltRounds)
+    return hashSync(receivedPassword, salt)
+}
 
 async function seedUsers() {
     // Perform database queries here
@@ -34,6 +40,40 @@ async function seedUsers() {
                         ]
                     },
                     body: "Next. js v13 was released by Vercel at the Next. js conference in October 2022, bringing many new features and improvements.",
+                    published: true
+                }, {
+                    title: 'Validate related schema attributes with Zod',
+                    slug: 'validate-related-schema-attributes-with-zod',
+                    tags: {
+                        create: [
+                            {
+                                name: "zod"
+                            },
+                            {
+                                name: "javascript"
+                            }
+                        ]
+                    },
+                    body: "<p>Zod is a TypeScript-first schema declaration and validation library.</p><p> I'm using the term 'schema' to broadly refer to any data type, from a simple string to a complex nested object.</p>",
+                    published: true
+                }, {
+                    title: 'Marketing for Developers: The Unconventional Guide ',
+                    slug: 'marketing-for-developers-the-unconventional-guide ',
+                    tags: {
+                        create: [
+                            {
+                                name: "tutorial"
+                            },
+                            {
+                                name: "coding"
+                            }
+                        ]
+                    },
+                    body: `<p>You've built an amazing new product. The code is elegant, the interface intuitive. You launch it with pride!</p>
+                    <br>
+                    <p>But then...tumbleweeds. Crickets. Where are all the users?</p>
+                    <br>
+                    <p>As developers, we excel at building products but often neglect marketing them. Let's change that!</p>`,
                     published: true
                 }]
             }
