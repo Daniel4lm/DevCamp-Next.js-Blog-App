5b123403b56875954c856fe2d9ac526f2b757c89
Daniel Molnar <daniel4molnar@gmail.com>
2023-08-30 09:02:50 +0200

    Change middleware routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,17 @@ import { NextRequestWithAuth, withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
 export default withAuth(
-    
+
     function middleware(req: NextRequestWithAuth) {
 
         const userRole = req.nextauth.token?.role
         const path = "/posts/new" || "/posts/edit" || "/settings/account" || "/settings/password"
 
-        if (req.nextUrl.pathname.includes(path) && !['user', 'admin'].includes(userRole || '')) {
+        if (req.nextUrl.pathname.includes(path) && !['USER', 'ADMIN'].includes(userRole || '')) {
             return NextResponse.rewrite(new URL('/denied', req.url))
         }
 
-        if (req.nextUrl.pathname.includes("/dashboard") && userRole !== 'admin') {
+        if (req.nextUrl.pathname.includes("/dashboard") && userRole !== 'ADMIN') {
             return NextResponse.rewrite(new URL('/denied', req.url))
         }
     },
@@ -26,10 +26,10 @@ export default withAuth(
 export const config = {
     // matcher: ["/((?!register|kapi|login).*)"],
     matcher: [
+        "/auth/logout",
         "/dashboard",
         "/posts/new",
         "/posts/edit",
-        "/settings/account/(.*)",
-        "/settings/password/(.*)"
+        "/settings/account/(.*)"
     ],
 }
