b4ba36251ff9a467fbc18902d7e8c4b470c4c577
Daniel Molnar <daniel4molnar@gmail.com>
2023-10-30 11:44:07 +0100

    Fix bugs & refactor code

--- src/components/forms/MultiTagSelect.tsx
+++ src/components/forms/MultiTagSelect.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, KeyboardEvent, MouseEvent, useRef, useState } from 'react'
+import React, { KeyboardEvent, MouseEvent, useRef } from 'react'
 import { CloseIcon } from '../Icons'
 
 function MultiTagSelect({ propTags = [], handleTags }: { propTags: string[], handleTags: (val: string[]) => void }) {
@@ -12,7 +12,7 @@ function MultiTagSelect({ propTags = [], handleTags }: { propTags: string[], han
 
     function handleTagInput(event: KeyboardEvent<HTMLInputElement>) {
         event.stopPropagation()
-        const inputValue = event.currentTarget.value.trim().replaceAll("#", '')
+        const inputValue = event.currentTarget.value.trim().replaceAll(/[#,\s+]/gi, '')
         if (['Tab', 'Space', 'Enter', 'NumpadEnter', 'Comma'].includes(event.code)) {
             if (inputValue.length && inputValue.length >= 2) {
                 const updatedTags = [...propTags, inputValue].reduce(
@@ -27,20 +27,22 @@ function MultiTagSelect({ propTags = [], handleTags }: { propTags: string[], han
     }
 
     return (
-        <div className="flex flex-wrap border border-gray-300 dark:bg-[#4a5469] dark:text-slate-100 dark:border-slate-400 focus-within:ring-2 focus-within:ring-indigo-400 dark:focus-within:ring-blue-400 dark:focus-within:border-transparent ring-opacity-90 focus-within:border-transparent overflow-hidden rounded-md bg-white">
+        <div className="flex flex-wrap border duration-500 border-[#5C5C5CB2] dark:bg-[#4a5469] dark:text-slate-100 dark:border-slate-400 focus-within:ring-2 focus-within:ring-indigo-400 dark:focus-within:ring-blue-400 dark:focus-within:border-transparent ring-opacity-90 focus-within:border-transparent overflow-hidden rounded-md bg-white">
             <div className="flex items-center flex-wrap flex-1 dark:bg-[#4a5469]">
                 {
                     propTags.map(topic => (
                         <div
                             id={`post-topic-${topic}`}
                             key={`post-topic-${topic}`}
-                            className="flex flex-row justify-center items-center rounded-lg text-sm md:text-base text-gray-800 text-center bg-slate-200 dark:bg-slate-300 p-2 m-1"
+                            data-test='post-topic-item'
+                            className="flex justify-center items-center rounded-lg text-sm md:text-base text-gray-800 text-center bg-slate-200 dark:bg-slate-300 p-2 m-1"
                         >
-                            #{topic}
+                            {`#${topic}`}
                             <div
                                 id={`remove-topic-${topic}`}
                                 data-remove={topic}
                                 onClick={removeTopic}
+                                data-testid={`remove-topic-${topic}`}
                                 className="w-4 h-4 cursor-pointer ml-2 rounded-full flex justify-center items-center
                                     hover:transition-all hover:duration-[600] hover:ease-in-out text-gray-500 hover:text-red-600"
                             >
@@ -51,8 +53,8 @@ function MultiTagSelect({ propTags = [], handleTags }: { propTags: string[], han
                 }
 
                 <input
-                    id='tags-input'
-                    name='title'
+                    id='tags'
+                    name='tags'
                     type='text'
                     ref={inpuRef}
                     className={'flex-1 px-2 py-3 border-0 dark:bg-[#4a5469] placeholder-gray-500 dark:placeholder-slate-400 outline-none focus:ring-0 text-sm md:text-base'}
