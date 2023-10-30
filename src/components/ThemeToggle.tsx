0d2425acead75e5ca8825de956854044d4036d4f
Daniel Molnar <daniel4molnar@gmail.com>
2023-10-30 11:42:47 +0100

    Wait until component is mounted

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,20 +2,30 @@
 
 import { useThemeContext } from '@/context/ThemeContext'
 import { MoonIcon, SunIcon } from './Icons'
+import { useEffect, useState } from 'react'
 
 export const ThemeToggle = () => {
     const { toggleMode } = useThemeContext()
 
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
     return (
-        <button
-            id="theme-toggle"
-            className="rounded-full border-2 border-transparent hover:border-slate-100 dark:ring-2 dark:border-none ring-slate-200 dark:ring-slate-500 hover:bg-slate-100 dark:hover:bg-slate-500"
-            onClick={() => toggleMode()}
-        >
-            <div className="flex items-center justify-center w-8 h-8 md:w-9 md:h-9">
-                <SunIcon />
-                <MoonIcon />
-            </div>
-        </button>
+        <>
+            <button
+                id="theme-toggle"
+                data-test="theme-toggle"
+                className="rounded-full border-2 border-transparent ring-2 dark:border-transparent ring-transparent dark:ring-slate-500 dark:hover:bg-slate-500"
+                onClick={mounted ? () => toggleMode() : undefined}
+            >
+                <div className="flex items-center justify-center w-8 h-8 md:w-9 md:h-9">
+                    <SunIcon />
+                    <MoonIcon />
+                </div>
+            </button>
+        </>
     )
 }
