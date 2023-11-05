7fc499951c0706cc4fb195ea0359c67d3fc15d74
Daniel Molnar <daniel4molnar@gmail.com>
2023-11-05 10:17:16 +0100

    Refactor navbar & app menus

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,69 +1,80 @@
-
-import Link from 'next/link'
-import { AppIcon } from '../Icons'
-import { ThemeToggle } from '../ThemeToggle'
-import { SettingsMenu } from './SettingsMenu'
-import ToolTip from '../Tooltip'
-import WorkMenu from './WorkMenu'
-import { User } from 'next-auth'
+import Link from "next/link";
+import { AppIcon } from "../Icons";
+import { ThemeToggle } from "../ThemeToggle";
+import { SettingsMenu } from "./SettingsMenu";
+import { ToolTip } from "../Tooltip";
+import WorkMenu from "./WorkMenu";
+import { User } from "next-auth";
 
 interface NavBarProps {
-    title?: string
-    currentUser?: User | undefined
+  title?: string;
+  currentUser?: User | undefined;
 }
 
 export default function Navbar({ title, currentUser }: NavBarProps) {
+  const userFontName =
+    currentUser?.fontName && currentUser?.fontName !== undefined
+      ? `${currentUser?.fontName}`
+      : "font-default";
 
-    return (
-        <header id="app-navbar" className="h-16 border-b dark:border-transparent flex fixed top-0 w-full bg-white dark:bg-navbar-dark dark:text-slate-100 z-50">
-            <div className="flex justify-between items-center px-2 py-0 mx-auto w-full md:w-11/12"> {/* 2xl:w-8/12 */}
-                <div className="flex justify-center items-center gap-2 xs:gap-0">
-
-                    <Link href="/" className="flex items-center gap-2 mx-4">
-                        <AppIcon />
-                        <h4 className="text-lg md:text-xl font-normal">{title}</h4>
-                    </Link>
-                </div>
-
-                <nav className="lg:w-3/5">
-                    <ul className="flex items-center justify-end pl-2 text-xs md:text-base">
-                        {currentUser
-                            ? (
-                                <>
-                                    <WorkMenu />
-
-                                    {/* <NotificationsComponent
-                                        id="notifications-comp"
-                                        current_user={@current_user}
-                                        /> */}
-                                    <SettingsMenu currentUser={currentUser} />
-                                </>
-                            )
-                            : (
-                                <>
-                                    <li className="hidden xs:block">
-                                        <Link href="/api/auth/signin" className="ml-2 py-[0.4rem] px-4 text-gray-700 rounded-lg font-medium duration-150 ease-in-out bg-transparent hover:underline dark:border-none dark:text-slate-100">
-                                            Log In
-                                        </Link>
-                                    </li>
-                                    <li className="hidden xs:block">
-                                        <Link href="/auth/register" className="ml-2 py-[0.4rem] px-4 text-gray-700 rounded-lg font-medium duration-150 ease-in-out bg-transparent border border-slate-300 dark:border-none hover:bg-slate-100 dark:hover:bg-slate-500 dark:text-slate-100">
-                                            Sign Up
-                                        </Link>
-                                    </li>
-                                </>
-                            )}
-                        <ToolTip position="bottom" title="Day/Night Theme">
-                            <li
-                                id="theme-switch"
-                                className="relative mx-2 xs:mx-4 text-gray-600 dark:text-gray-200 hover:text-indigo-400"
-                            >
-                                <ThemeToggle />
-                            </li>
-                        </ToolTip>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header
+      id="app-navbar"
+      className={`h-16 ${userFontName} border-b dark:border-transparent flex fixed top-0 w-full bg-white dark:bg-navbar-dark dark:text-slate-100 z-50`}
+    >
+      <div className="flex justify-between items-center px-2 py-0 mx-auto w-full md:w-11/12">
+        {" "}
+        <div className="flex justify-center items-center gap-2 xs:gap-0">
+          <Link
+            aria-label="Logo"
+            data-test="logo"
+            href="/"
+            className="flex items-center gap-2 mx-4"
+          >
+            <AppIcon />
+            <h4 className="text-lg md:text-xl font-normal">{title}</h4>
+          </Link>
+        </div>
+        <nav className="lg:w-3/5">
+          <ul className="flex items-center justify-end pl-2 text-xs md:text-base">
+            {currentUser ? (
+              <>
+                <WorkMenu />
+                <SettingsMenu currentUser={currentUser} />
+              </>
+            ) : (
+              <>
+                <li className="hidden xs:block">
+                  <Link
+                    data-test="login-link"
+                    href="/api/auth/signin"
+                    className="ml-2 py-[0.4rem] px-4 text-gray-700 hover:text-indigo-500 decoration-2 rounded-full hover:border-indigo-400 font-medium duration-150 ease-in-out bg-transparent hover:underline dark:border-transparent dark:text-slate-100"
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li className="hidden xs:block">
+                  <Link
+                    data-test="register-link"
+                    href="/auth/register"
+                    className="ml-2 py-[0.4rem] px-4 text-gray-700 rounded-full font-medium duration-150 ease-in-out bg-transparent border border-slate-300 dark:border-transparent hover:border-indigo-400 hover:text-indigo-600 dark:hover:bg-slate-500 dark:text-slate-100"
+                  >
+                    Sign Up
+                  </Link>
+                </li>
+              </>
+            )}
+            <ToolTip position="bottom" title="Day/Night Theme">
+              <li
+                id="theme-switch"
+                className="relative mx-2 xs:mx-4 text-gray-600 dark:text-gray-200 hover:text-[#755FFF]"
+              >
+                <ThemeToggle />
+              </li>
+            </ToolTip>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
--- src/components/navigation/SettingsMenu.tsx
+++ src/components/navigation/SettingsMenu.tsx
@@ -1,123 +1,203 @@
-"use client"
+"use client";
 
-import { useEffect, useRef, useState } from 'react'
-import Link from 'next/link'
-import useOutsideClick from '@/hooks/useOutsideClick'
-import { CloseSideMenuIcon, LogoutIcon, SettingsIcon2, SmallUserIcon, TagListIcon } from '../Icons'
-import SideMenu from './SideMenu'
-import { AvatarLink } from '../CoreComponents'
-import { User } from 'next-auth'
+import { MouseEvent, useEffect, useRef, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { User } from "next-auth";
+import useOutsideClick from "@/hooks/useOutsideClick";
+import {
+  CloseSideMenuIcon,
+  LogoutIcon,
+  SettingsIcon2,
+  SmallUserIcon,
+  TagListIcon,
+} from "../Icons";
+import { MemoizedSideMenu } from "./SideMenu";
+import { AvatarLink } from "../CoreComponents";
+import { WelcomeTooltip } from "../Tooltip";
 
 enum MenuState {
-    close = 'close',
-    open = 'open'
+  close = "close",
+  open = "open",
 }
 
 export function SettingsMenu({ currentUser }: { currentUser: User }) {
-    const [menuOpen, setMenuOpen] = useState<MenuState>(MenuState.close)
-    const avatarRef = useRef(null)
-    useOutsideClick(avatarRef, closeMenu)
+  const [menuOpen, setMenuOpen] = useState<MenuState>(MenuState.close);
+  const { data: session, update: updateSession } = useSession();
+  const avatarRef = useRef(null);
+  const router = useRouter();
+  useOutsideClick(avatarRef, closeMenu);
 
-    function handleMenuOpen() {
-        setMenuOpen(menuOpen === MenuState.open ? MenuState.close : MenuState.open)
-    }
+  function handleMenuOpen() {
+    setMenuOpen(menuOpen === MenuState.open ? MenuState.close : MenuState.open);
+  }
 
-    function closeMenu() {
-        setMenuOpen(MenuState.close)
-    }
+  function closeMenu() {
+    setMenuOpen(MenuState.close);
+  }
 
-    useEffect(() => {
-        const handleEscape = (event: KeyboardEvent) => {
-            if (event.key === 'Escape') closeMenu()
-        }
+  const closeTooltip = async () => {
+    //event.stopPropagation()
 
-        document.addEventListener('keydown', handleEscape)
+    const formData = new FormData();
+    formData.append("id", currentUser.id);
+    formData.append("profileVisited", "true");
 
-        return () => document.removeEventListener('keydown', handleEscape)
-    }, [])
+    const updateProfileRes = await fetch(
+      `${process.env.NEXT_PUBLIC_SITE_URL}/api/users/${currentUser.username}/profile`,
+      {
+        method: "PUT",
+        body: formData,
+      }
+    );
 
-    return (
-        <>
-            <li className="xs:relative mx-2" ref={menuOpen === MenuState.open ? avatarRef : null}>
-                <div
-                    id="user-avatar"
-                    className="cursor-pointer flex items-center justify-center text-neutral-600 flex-col dark:text-gray-200 hover:text-black hover:underline"
-                    onClick={handleMenuOpen}
-                >
-                    <AvatarLink src={currentUser?.avatarUrl || ''} className="w-8 h-8 md:w-9 md:h-9 ring-2 ring-slate-200 dark:ring-slate-500 hover:bg-gray-100 dark:hover:bg-slate-500 hover:ring-[#B1B8F8] hover:ring-opacity-80" />
-                    {/* {<span className='hidden xs:block'>Me</span>} */}
+    const userProfileData = await updateProfileRes.json();
+
+    await updateSession({
+      ...session,
+      user: {
+        ...(typeof session?.user === "object" ? session?.user : {}),
+        ...{
+          profileVisited: userProfileData.updatedProfile.profileVisited,
+        },
+      },
+    });
+
+    //router.refresh()
+  };
+
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key === "Escape") closeMenu();
+    };
+
+    document.addEventListener("keydown", handleEscape);
+
+    return () => document.removeEventListener("keydown", handleEscape);
+  }, []);
+
+  return (
+    <>
+      <li
+        className="xs:relative mx-2"
+        ref={menuOpen === MenuState.open ? avatarRef : null}
+      >
+        <div
+          id="user-avatar"
+          data-test="user-avatar-link"
+          className="relative cursor-pointer flex items-center justify-center text-neutral-600 flex-col dark:text-gray-200 hover:text-black hover:underline"
+          onClick={handleMenuOpen}
+        >
+          <AvatarLink
+            src={currentUser?.avatarUrl || ""}
+            className="relative w-8 h-8 md:w-9 md:h-9 ring-2 ring-slate-200 dark:ring-slate-500 hover:bg-gray-100 dark:hover:bg-slate-500 hover:ring-[#B1B8F8] hover:ring-opacity-80"
+          />
+          {/* {<span className='hidden xs:block'>Me</span>} */}
+          {!currentUser.profileVisited ? (
+            <WelcomeTooltip closeTooltip={closeTooltip} />
+          ) : null}
+        </div>
+
+        <MemoizedSideMenu
+          id="settings-menu"
+          classNames="w-full h-full shadow dark:text-white border-l border-slate-300 dark:border-slate-600 bg-white dark:dark:bg-menu-dark-github overflow-auto"
+          isOpen={menuOpen}
+          side="right"
+          closeFunc={closeMenu}
+        >
+          <ul
+            id="settings-menu"
+            data-test="settings-menu-list"
+            className={`absolute w-full p-2 bg-white dark:bg-menu-dark-github dark:text-slate-100 overflow-hidden text-sm`}
+          >
+            <li className="py-2 px-2 flex items-center justify-between">
+              <div className="flex items-center flex-1">
+                <AvatarLink
+                  withLink={`/user/${currentUser.username}`}
+                  src={currentUser?.avatarUrl || ""}
+                  className={
+                    "max-w-[2.1rem] max-h-[2.1rem] xs:max-w-[2.4rem] xs:max-h-[2.4rem] ring-2 ring-slate-200 dark:ring-slate-500 hover:bg-gray-100 dark:hover:bg-slate-500 hover:ring-[#B1B8F8] hover:ring-opacity-80"
+                  }
+                />
+                <div className="flex-1 ml-2">
+                  <p
+                    data-test="user-name-title"
+                    className="text-lg font-medium"
+                  >
+                    {currentUser?.fullName}
+                  </p>
+                  <span data-test="user-email-title">{currentUser?.email}</span>
                 </div>
-
-                <SideMenu
-                    id='settings-menu'
-                    classNames='w-full h-full shadow dark:text-white border-l border-slate-300 dark:border-slate-600 bg-white dark:dark:bg-menu-dark-github overflow-auto'
-                    isOpen={menuOpen}
-                    side='right'
-                    closeFunc={closeMenu}
-                >
-                    <ul
-                        id="settings-menu"
-                        className={`absolute w-full p-2 bg-white dark:bg-menu-dark-github dark:text-slate-100 overflow-hidden text-sm`}
-                    >
-                        <li className="py-2 px-2 flex items-center justify-between">
-                            <div className="flex items-center flex-1">
-                                <AvatarLink
-                                    withLink={`/user/${currentUser.username}`}
-                                    src={currentUser?.avatarUrl || ''}
-                                    className={"max-w-[2.1rem] max-h-[2.1rem] xs:max-w-[2.4rem] xs:max-h-[2.4rem] ring-2 ring-slate-200 dark:ring-slate-500 hover:bg-gray-100 dark:hover:bg-slate-500 hover:ring-[#B1B8F8] hover:ring-opacity-80"}
-                                />
-                                <div className="flex-1 ml-2">
-                                    <p className="text-lg font-medium">{currentUser?.fullName}</p>
-                                    <span>{currentUser?.email}</span>
-                                </div>
-                            </div>
-                            <a
-                                id="mobile-menu-close"
-                                href="#"
-                                className="z-50 dark:text-slate-200 hover:bg-[#e4e6fc] hover:dark:bg-slate-600 cursor-pointer rounded-lg p-2"
-                                onClick={closeMenu}
-                            >
-                                <CloseSideMenuIcon />
-                            </a>
-                        </li>
-                        <hr className="my-2 dark:border-gray-600" />
-                        <Link href={`/user/${currentUser?.username}`} onClick={closeMenu}>
-                            <li className="flex justify-start items-center gap-2 p-2 rounded-md hover:bg-indigo-50 dark:hover:bg-slate-600">
-                                <span className='text-slate-800 dark:text-inherit'>
-                                    <SmallUserIcon />
-                                </span>
-                                <span>My profile</span>
-                            </li>
-                        </Link>
-
-                        <Link href="/user/saved-list" onClick={closeMenu}>
-                            <li className="flex justify-start items-center gap-2 p-2 rounded-md hover:bg-indigo-50 dark:hover:bg-slate-600">
-                                <span className='text-slate-800 dark:text-inherit'>
-                                    {/* {<SavedListIcon classNames='w-4 h-4' />} */}
-                                    <TagListIcon classNames='w-4 h-4' />
-                                </span>
-                                <span>Saved list</span>
-                            </li>
-                        </Link>
-
-                        <Link href={`/settings/account/${currentUser.username}`} onClick={closeMenu}>
-                            <li className="flex justify-start items-center gap-2 p-2 rounded-md hover:bg-indigo-50 dark:hover:bg-slate-600">
-                                <span className='text-slate-800 dark:text-inherit'><SettingsIcon2 /></span>
-                                <span>Settings</span>
-                            </li>
-                        </Link>
-
-                        <hr className="my-2 dark:border-gray-600" />
-
-                        <Link href={`/api/auth/signout`} onClick={closeMenu}>
-                            <li className="flex justify-start items-center gap-2 p-2 cursor-pointer rounded-md hover:bg-indigo-50 dark:hover:bg-slate-600">
-                                <span className='text-slate-800 dark:text-inherit'><LogoutIcon /></span>
-                                <span>Log Out</span>
-                            </li>
-                        </Link>
-                    </ul >
-                </SideMenu>
+              </div>
+              <a
+                id="mobile-menu-close"
+                href="#"
+                data-test="close-menu-link"
+                className="z-50 dark:text-slate-200 hover:bg-[#e4e6fc] hover:dark:bg-slate-600 cursor-pointer rounded-lg p-2"
+                onClick={closeMenu}
+              >
+                <CloseSideMenuIcon />
+              </a>
             </li>
-        </>
-    )
+            <hr className="my-2 dark:border-gray-600" />
+            <Link
+              data-test="my-profile-link"
+              href={`/user/${currentUser?.username}?tab=all`}
+              onClick={closeMenu}
+            >
+              <li className="flex justify-start items-center gap-2 p-2 rounded-md hover:bg-indigo-50 dark:hover:bg-slate-600">
+                <span className="text-slate-800 dark:text-inherit">
+                  <SmallUserIcon />
+                </span>
+                <span>My profile</span>
+              </li>
+            </Link>
+
+            <Link
+              data-test="saved-list-link"
+              href={`/user/${currentUser?.username}?tab=starred`}
+              onClick={closeMenu}
+            >
+              <li className="flex justify-start items-center gap-2 p-2 rounded-md hover:bg-indigo-50 dark:hover:bg-slate-600">
+                <span className="text-slate-800 dark:text-inherit">
+                  {/* {<SavedListIcon classNames='w-4 h-4' />} */}
+                  <TagListIcon classNames="w-4 h-4" />
+                </span>
+                <span>Saved list</span>
+              </li>
+            </Link>
+
+            <Link
+              data-test="settings-link"
+              href={`/settings/account/${currentUser.username}`}
+              onClick={closeMenu}
+            >
+              <li className="flex justify-start items-center gap-2 p-2 rounded-md hover:bg-indigo-50 dark:hover:bg-slate-600">
+                <span className="text-slate-800 dark:text-inherit">
+                  <SettingsIcon2 />
+                </span>
+                <span>Settings</span>
+              </li>
+            </Link>
+
+            <hr className="my-2 dark:border-gray-600" />
+
+            <Link
+              data-test="logout-link"
+              href={`/api/auth/signout`}
+              onClick={closeMenu}
+            >
+              <li className="flex justify-start items-center gap-2 p-2 cursor-pointer rounded-md hover:bg-indigo-50 dark:hover:bg-slate-600">
+                <span className="text-slate-800 dark:text-inherit">
+                  <LogoutIcon />
+                </span>
+                <span>Log Out</span>
+              </li>
+            </Link>
+          </ul>
+        </MemoizedSideMenu>
+      </li>
+    </>
+  );
 }
